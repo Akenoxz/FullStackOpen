fc0ddc5df67d7c515fc9acff90bb491f01885cf3
Akenoxz <thomas.laranjo841@gmail.com>
2024-01-19 19:25:28 +0200

    part 1.3

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ name, exercises }) => <p>{name} {exercises}</p>;
-
-const Content = ({ parts }) => (
-  <div>
-    <Part name={parts[0].name} exercises={parts[0].exercises} />
-    <Part name={parts[1].name} exercises={parts[1].exercises} />
-    <Part name={parts[2].name} exercises={parts[2].exercises} />
-  </div>
-);
-
-const Total = ({ parts }) => (
-  <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-);
-
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts[0].name}: {course.parts[0].exercises}
+      </p>
+      <p>
+        {course.parts[1].name}: {course.parts[1].exercises}
+      </p>
+      <p>
+        {course.parts[2].name}: {course.parts[2].exercises}
+      </p>
+      <p>
+        Total exercises: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+      </p>
     </div>
-  );
-};
-
-export default App;
-
-/*z */
+  )
+}
